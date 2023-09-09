aac74142e1134678e99d238770b1f17a41d0539f
Elnur <entreur@gmail.com>
2023-09-09 19:52:03 +0400

    User data being sent to dashboard now

--- Project/Areas/Admin/Controllers/DashboardController.cs
+++ Project/Areas/Admin/Controllers/DashboardController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KanunWebsite.Areas.Admin.Filter;
+using KanunWebsite.Areas.Admin.ViewModel;
+using KanunWebsite.Data;
+using KanunWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KanunWebsite.Areas.Admin.Controllers
 {
     [Area("admin")]
     public class DashboardController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [TypeFilter(typeof(Auth))]
         public IActionResult Index()
         {
-            return View();
+            User? usr = ReturnUserData();
+            VMUser dashboard = new() {
+                Fullname = usr.FullName,
+                Token = usr.Token,
+                Email = usr.Email,
+                ProfileImage = usr.ProfilePicture,
+            };
+            return View(dashboard);
         }
         public IActionResult Users()
         {
@@ -15,7 +32,20 @@ namespace KanunWebsite.Areas.Admin.Controllers
         }
         public IActionResult Blog()
         {
-            return View();
+            User? usr = ReturnUserData();
+            VMUser dashboard = new()
+            {
+                Fullname = usr.FullName,
+                Token = usr.Token,
+                Email = usr.Email,
+                ProfileImage = usr.ProfilePicture,
+            };
+            return View(dashboard);
+        }
+
+        private User? ReturnUserData()
+        {
+            return _context.Users.Where(u => u.Token == Request.Cookies["token"]).First();
         }
     }
 }
