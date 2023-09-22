b038a609e0602e655b14d8908f2e08516a697804
Elnur <entreur@gmail.com>
2023-09-22 14:49:01 +0400

    FAQ CRUD System. Implemented

--- Project/Areas/Admin/Controllers/QAController.cs
+++ Project/Areas/Admin/Controllers/QAController.cs
@@ -11,6 +11,7 @@ namespace KanunWebsite.Areas.Admin.Controllers
     [TypeFilter(typeof(Auth))]
     public class QAController : Controller
     {
+        private readonly FAQ QAInsert = new();
         private readonly ApplicationDbContext _context;
         public QAController(ApplicationDbContext context)
         {
@@ -31,23 +32,80 @@ namespace KanunWebsite.Areas.Admin.Controllers
             return View(newsletter);
         }
 
-        [HttpPost]
+        [HttpGet]
         public IActionResult Create()
         {
-            throw new NotImplementedException();
+            User? usr = ReturnUserData();
+            VMAdminCreateQA data = new()
+            {
+                Fullname = usr.FullName,
+                Token = usr.Token,
+                Email = usr.Email,
+                ProfileImage = usr.ProfilePicture,
+            };
+            return View(data);  
         }
 
-        [HttpPut]
-        public IActionResult Edit()
+        [HttpPost]
+        public IActionResult Create(VMAdminCreateQA qa)
         {
-            throw new NotImplementedException();
+            #region Data Binding
+            QAInsert.CardId = qa.CardId;
+            QAInsert.Question = qa.Question;
+            QAInsert.Answer = qa.Answer;
+            QAInsert.IsHidden = qa.IsHidden;
+            #endregion
+            if (ModelState.IsValid)
+            {
+                _context.Add(QAInsert);
+                _context.SaveChanges();
+                return RedirectToAction("index", "qa");
+            }
+            throw new Exception("Model State Is Not Valid");
         }
 
-        [HttpDelete]
-        public IActionResult Delete()
+        [HttpGet("{id}")]
+        public IActionResult Edit(int id)
         {
-            return View();
+            FAQ? CurrentIteration = _context.FAQs.FirstOrDefault(f => f.Id == id);
+            User? usr = ReturnUserData();
+            VMAdminEditQA data = new()
+            {
+                Fullname = usr.FullName,
+                Token = usr.Token,
+                Email = usr.Email,
+                ProfileImage = usr.ProfilePicture,
+            };
+            ViewBag.FAQ = CurrentIteration;
+            return View(data);
         }
+
+        [HttpPost("{id}")]
+        public IActionResult Edit(int id, VMAdminEditQA ViewModel)
+        {
+            FAQ? QAModel = _context.FAQs.FirstOrDefault(f => f.Id == id);
+            #region Data Binding
+            QAModel.CardId = ViewModel.CardId;
+            QAModel.Question = ViewModel.Question;
+            QAModel.Answer = ViewModel.Answer;
+            QAModel.IsHidden = ViewModel.IsHidden;
+            #endregion
+            if (ModelState.IsValid)
+            {
+                _context.SaveChanges();
+                return RedirectToAction("index", "qa");
+            }
+            throw new Exception("Model State is not valid.");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            FAQ? QA = _context.FAQs.FirstOrDefault(f => f.Id == id);
+            _context.FAQs.Remove(QA);
+            _context.SaveChanges();
+            return RedirectToAction("index", "qa");
+        }
+
         private User? ReturnUserData()
         {
             return _context.Users.Where(u => u.Token == Request.Cookies["token"]).FirstOrDefault();
--- Project/Areas/Admin/ViewModelAdmin/VMAdminQA.cs
+++ Project/Areas/Admin/ViewModelAdmin/VMAdminQA.cs
@@ -1,10 +1,41 @@
 ﻿using KanunWebsite.Models;
-using KanunWebsite.ViewModels;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KanunWebsite.Areas.Admin.ViewModelAdmin
 {
     public class VMAdminQA:VMAdminBase
     {
         public List<FAQ>? Questions { get; set; }    
+    }
+    public class VMAdminCreateQA:VMAdminBase 
+    {
+        [Required]
+        [MaxLength(250, ErrorMessage = "Cannot exceed 250 letters.")]
+        [MinLength(10, ErrorMessage = "Cannot be less than 10 letters.")]
+        public string? Question { get; set; }
+        [Required]
+        [MaxLength(250, ErrorMessage = "Cannot exceed 250 letters.")]
+        [MinLength(10, ErrorMessage = "Cannot be less than 10 letters.")]
+        public string? Answer { get; set; }
+        public bool IsHidden { get; set; } = false;
+        [Required]
+        [MaxLength(10, ErrorMessage = "Cannot exceed 10 letters.")]
+        public string? CardId { get; set; }
+    }
+    public class VMAdminEditQA:VMAdminBase
+    {
+        [Required]
+        [MaxLength(250, ErrorMessage = "Cannot exceed 250 letters.")]
+        [MinLength(10, ErrorMessage = "Cannot be less than 10 letters.")]
+        public string? Question { get; set; }
+        [Required]
+        [MaxLength(250, ErrorMessage = "Cannot exceed 250 letters.")]
+        [MinLength(10, ErrorMessage = "Cannot be less than 10 letters.")]
+        public string? Answer { get; set; }
+        public bool IsHidden { get; set; } = false;
+        [Required]
+        [MaxLength(10, ErrorMessage = "Cannot exceed 10 letters.")]
+        public string? CardId { get; set; }
     }
 }
