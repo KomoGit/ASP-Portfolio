342175f3270e98ec22282ad6e252ff18df961aff
Elnur <entreur@gmail.com>
2023-09-22 14:53:49 +0400

    Adjustments to annotations

--- Project/Models/Testimonial.cs
+++ Project/Models/Testimonial.cs
@@ -6,19 +6,17 @@ namespace KanunWebsite.Models
     {
         [Required]
         [MaxLength(120,ErrorMessage = "Cannot exceed 120 letters.")]
-        [MinLength(10,ErrorMessage = "Cannot be less than 10 letters.")]
+        [MinLength(5,ErrorMessage = "Cannot be less than 10 letters.")]
         public string? Fullname { get; set; }
         [Required]
         [MaxLength(30, ErrorMessage = "Cannot exceed 30 letters.")]
-        [MinLength(10, ErrorMessage = "Cannot be less than 10 letters.")]
+        [MinLength(5, ErrorMessage = "Cannot be less than 10 letters.")]
         public string? Title { get; set; }
         [Required]
         [MaxLength(120, ErrorMessage = "Cannot exceed 120 letters.")]
-        [MinLength(10, ErrorMessage = "Cannot be less than 10 letters.")]
+        [MinLength(5, ErrorMessage = "Cannot be less than 10 letters.")]
         public string? Description { get; set; }
-        [Required]
-        [MaxLength(50, ErrorMessage = "Cannot exceed 50 letters.")]
-        [MinLength(10, ErrorMessage = "Cannot be less than 10 letters.")]
         public string? Image { get; set; }
+        public bool IsHidden { get; set; } = false;
     }
 }
