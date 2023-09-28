b64a174bcc560e073a87b33c83911651e58f568c
Elnur <entreur@gmail.com>
2023-09-28 18:51:34 +0400

    Login limit

--- Project/Areas/Admin/Filter/Auth.cs
+++ Project/Areas/Admin/Filter/Auth.cs
@@ -1,5 +1,7 @@
-﻿using KanunWebsite.Data;
+﻿using CryptoHelper;
+using KanunWebsite.Data;
 using KanunWebsite.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -8,22 +10,21 @@ namespace KanunWebsite.Areas.Admin.Filter
     public class Auth:ActionFilterAttribute
     {
         private readonly ApplicationDbContext _context;
-
         public Auth(ApplicationDbContext context)
         {
             _context = context;
         }
-
+        //Using this system allows others to hijack tokens. Meaning if I use the token of someone else I can login without requiring password or email.
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            if(!context.HttpContext.Request.Cookies.TryGetValue("token", out string token))
+            if (!context.HttpContext.Request.Cookies.TryGetValue("token", out string token))
             {
-                context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { action = "login",controller = "admin" }));
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { action = "login", controller = "admin" }));
             }
-            User user = _context.Users.FirstOrDefault(u => u.Token == token);
+            User user = _context.Users.FirstOrDefault(u =>  u.Token == token);
             if (user == null)
             {
-                _ = new RedirectToRouteResult(new RouteValueDictionary(new { action = "login", controller = "admin" }));
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { action = "login", controller = "admin" }));
             }
             if (context.Controller is Controller controller)
             {
